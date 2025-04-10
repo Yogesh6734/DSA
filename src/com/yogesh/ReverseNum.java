4224b401427512489bda034442c5ae863b4233e2
Yogesh <yogesh.bhati@indiamart.com>
2025-04-11 00:43:11 +0530

    first commit

---
+++ src/com/yogesh/ReverseNum.java
@@ -0,0 +1,39 @@
+package com.yogesh;
+
+public class ReverseNum {
+    public static void main(String[] args) {
+       int a= rev1(2231);
+        System.out.println(a);
+        System.out.println(countZeros(30002300,0));
+    }
+    static int sum=0;
+    public static void rev(int n){
+
+        if(n==0){
+           return;
+        }
+        sum=sum*10 + n%10;
+        rev(n/10);
+    }
+
+    public static int rev1(int n){
+
+        if(n%10==0){
+            return n;
+        }
+
+        return (n%10 * (int)Math.pow(10,(int)Math.log10(n))) + rev1(n/10);
+    }
+
+
+    static int countZeros(int n ,int c){
+        if(n==0){
+            return c;
+        }
+        if (n%10==0){
+            return countZeros(n/10,c+1);
+        }
+        return countZeros(n/10,c);
+    }
+}
+
